a46308bc4de62aa6c90fa67a300c471a82e0e4cc
Feimi <yuf2@rpi.edu>
2017-03-31 23:33:49 -0400

    pumi embeded;stiffness assemble complete

---
+++ build/CMakeFiles/fem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fem.dir/src/main.cc.o"
+  "fem.pdb"
+  "fem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
